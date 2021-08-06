cfbd3d8b3cc7e93463cbe622d48fa834fac35876
ignaciosein@gmail.com <ignaciosein@gmail.com>
2021-08-06 15:32:08 +0200

    solved code

--- app.js
+++ app.js
@@ -6,17 +6,13 @@ const cookieParser = require("cookie-parser");
 //require .env
 /* require("dotenv").config(); */
 
-const path = require('path');
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-// Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-// ** MIDDLEWARE ** //
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+ 
  
 // require rutas
 const router = require("./routes/accessibility");
